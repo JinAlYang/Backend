a1513a667f691e53fa5bb32ed1b1197da83d1d3d
sejoon00 <parksj313@konkuk.ac.kr>
2023-05-28 20:28:16 +0900

    [Feat : region data 추가 2]

--- src/main/java/com/JinAlYang/region/RegionDataInitializer.java
+++ src/main/java/com/JinAlYang/region/RegionDataInitializer.java
@@ -1,2 +1,26 @@
-package com.JinAlYang.region;public class RegionDataInitializer {
+package com.JinAlYang.region;
+
+import com.JinAlYang.region.domain.Region;
+import com.JinAlYang.region.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RegionDataInitializer implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 초기 데이터 생성 및 저장
+        Region region = Region.builder()
+                    .id(1L)
+                    .name("광진구")
+                    .road_address("서울특별시 광진구 자양로 117 광진구청")
+                    .build();
+
+        regionRepository.save(region);
+    }
 }
