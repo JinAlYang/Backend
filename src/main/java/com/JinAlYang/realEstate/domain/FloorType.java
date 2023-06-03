0d6d0bfbba91ffd661285dcb36cef9180b295ce3
jbaem <101052774+jbaem@users.noreply.github.com>
2023-06-04 01:23:08 +0900

    [Feat: 검색 기능 추가]

--- src/main/java/com/JinAlYang/realEstate/domain/FloorType.java
+++ src/main/java/com/JinAlYang/realEstate/domain/FloorType.java
@@ -1,21 +1,18 @@
 package com.JinAlYang.realEstate.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum MonthlyRentType {
-    MONTHLY("MONTHLY"), JEONSE("JEONSE");
+public enum FloorType {
+    ROOFTOP("옥탑"), GROUND("지상층"), UNDER("반지하");
 
     @Getter
     private String value;
 
-    MonthlyRentType(String value) {
-        this.value = value;
-    }
+    FloorType(String value) { this.value = value; }
 
     @JsonValue
     public String getValue() {
--- src/main/java/com/JinAlYang/realEstate/repository/RealEstateDetailRepository.java
+++ src/main/java/com/JinAlYang/realEstate/repository/RealEstateDetailRepository.java
@@ -3,6 +3,8 @@ package com.JinAlYang.realEstate.repository;
 import com.JinAlYang.realEstate.domain.RealEstate;
 import com.JinAlYang.realEstate.domain.RealEstateDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface RealEstateDetailRepository extends JpaRepository<RealEstateDetail, Long> {
+public interface RealEstateDetailRepository extends JpaRepository<RealEstateDetail, Long>, JpaSpecificationExecutor<RealEstate> {
 }
